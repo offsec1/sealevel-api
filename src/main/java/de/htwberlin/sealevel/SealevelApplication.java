a1a2de4bdcec6d09b06c54bef942a707f0b9fe9c
Patrick Martin <p.martin@funkedigital.de>
2020-05-16 15:29:47 +0200

    init commit

---
+++ src/main/java/de/htwberlin/sealevel/SealevelApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.sealevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SealevelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SealevelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/sealevel/SealevelApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.sealevel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SealevelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
